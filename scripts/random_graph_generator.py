2a48fb19b99af856c4d57f2f42116e09a8b87833
Leonardo Maccari <leonardo.maccari@unitn.it>
2014-11-06 16:28:06 +0100

    added random graph generator for tests

---
+++ scripts/random_graph_generator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import networkx as nx
+import sys
+
+if len(sys.argv) != 4:
+    print "usage: ./random_graph_generator.py numGraphs prefix numNodes"
+    sys.exit(1)
+
+numGraphs = int(sys.argv[1])
+prefix = sys.argv[2]
+numNodes = int(sys.argv[3])
+
+for i in range(numGraphs):
+    g = nx.fast_gnp_random_graph(numNodes,0.5)
+    if nx.is_connected(g):
+        nx.write_edgelist(g, prefix+"-"+str(i)+".edges")
+
