e873bd311e13640c41c8fa9b6370f6dd90698dfc
Leonardo Maccari <leonardo.maccari@unitn.it>
2015-02-04 18:05:02 +0100

    added methods to config inheritance class

--- inherit_config_parser.py
+++ inherit_config_parser.py
@@ -16,8 +16,8 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
-@copyright: Copyright (c) 2011, w3agency.net
-@author: Thomas Stachl <t.stachl@w3agency.net>
+@copyright: Copyright (c) 2011, w3agency.net, Leonardo Maccari
+@author: Thomas Stachl <t.stachl@w3agency.net>, Leonardo Maccari leonardo.maccari@disi.unitn.it
 @since: Mar 28, 2011
 """ 
 
@@ -35,3 +35,38 @@ class InheritConfigParser(SafeConfigParser):
                         return SafeConfigParser.get(self, s, option)
                     except NoOptionError:
                         return SafeConfigParser.get(self, s.split(":")[1].strip(), option) 
+
+    def sections(self):
+        real_sections = SafeConfigParser.sections(self)
+        parsed_sections = []
+        for section in real_sections:
+            if ":" not in section:
+                parsed_sections.append(section)
+            else:
+                parsed_sections.append(section.split(":")[0])
+        return parsed_sections
+
+    def items(self, section):
+        try:
+            return SafeConfigParser.items(self, section)
+        except NoSectionError:
+            for s in self._sections:
+                if s.find(section) != -1:
+                    # this section name includes the section name we're looking
+                    # for
+                    base_step = []
+                    try:
+                        # is it an existing section name? yes, then we
+                        # navigated to the highest ancstor, return the contents
+                        # of the stanza
+                        return SafeConfigParser.items(self, section)
+                    except NoSectionError:
+                        # no section is named like this, add the content of the
+                        # current stanza and climb to the father
+                        base_step = SafeConfigParser.items(self, s)
+                        return self.items(
+                                s.split(":")[1].strip()) + base_step
+
+
+
+
