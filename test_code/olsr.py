3b61ee2e63a935a0935caf969e39092df5789eed
Leonardo Maccari <leonardo.maccari@unitn.it>
2015-02-20 11:56:34 +0100

    general rework and multiple pings

--- test_code/olsr.py
+++ test_code/olsr.py
@@ -2,13 +2,10 @@
 import sys
 sys.path.append('../')
 from network_builder import *
-from os import kill, path, makedirs
+from os import kill, path
 from matplotlib.pyplot import ion
-from collections import defaultdict
 import random 
 import time
-import ast
-import networkx as nx
 from dummyrouting import dummyRoutingRandomTest
 
 import signal
@@ -35,30 +32,28 @@ class OLSRTest(dummyRoutingRandomTest):
     def save_config_file(self, host, args):
         return 
 
-    def launch_ping(self, host,  args):
+    def launch_ping(self, host, dest_array, run_id=0):
 
-        cmd = "ping " + args
+        for idx, ip in enumerate(dest_array):
+            cmd = "ping " + ip
 
-        log_str = "Host " + host.name + " lounching command:\n"
-        info(log_str)
-        info(cmd + "\n")
-        logfile = self.prefix + host.name + "_ping.log"
-
-        params = {}
-        params['>'] = logfile
-        params['2>'] = logfile
-
-
-        return self.bgCmd(host, True, cmd,
-            *reduce(lambda x, y: x + y, params.items()))
-
+            log_str = "Host " + host.name + " launching command:\n"
+            info(log_str)
+            info(cmd + "\n")
+            logfile = self.prefix + host.name + \
+                    "_ping_" + str(idx) + "_runId_" + str(run_id) +  ".log"
+            params = {}
+            params['>'] = logfile
+            params['2>'] = logfile
+            self.bgCmd(host, True, cmd, 
+                    *reduce(lambda x, y: x + y, params.items()))
 
     def launch_OLSR(self, host,  args):
 
         cmd = "../olsrd-0.6.8/olsrd " + \
                 args
 
-        log_str = "Host " + host.name + " lounching command:\n"
+        log_str = "Host " + host.name + " launching command:\n"
         info(log_str)
         info(cmd + "\n")
         logfile = self.prefix + host.name + ".log"
@@ -75,106 +70,64 @@ class OLSRTest(dummyRoutingRandomTest):
 
         info("*** Launching OLSR test\n")
         info("Data folder: "+self.prefix+"\n")
-        self.startRun()
-        sleep(self.duration)
-        self.killAll()
 
-    #def runTest(self):
+        
+        if self.stopAllNodes:
+            if type(self.stopAllNodes) == int:
+                self.centList = self.getCentrality()[:self.stopAllNodes]
+            elif type(self.stopAllNodes) == list:
+                for idx in self.stopAllNodes:
+                    self.centList.append(self.getCentrality()[idx])
 
-    #    info("*** Launching OLSR test\n")
-    #    info("Data folder: "+self.prefix+"\n")
+        for run_id in range(len(self.centList)):
+            info("\nStarting run " + str(run_id) + "\n")
+            if self.stopAllNodes:
+                self.nodeCrashed = self.centList.pop(0)
 
-    #    
-    #    if self.stop_all_nodes:
-    #        if type(self.stop_all_nodes) == int:
-    #            self.centList = self.getCentrality()[:self.stop_all_nodes]
-    #        elif type(self.stop_all_nodes) == list:
-    #            for idx in self.stop_all_nodes:
-    #                self.centList.append(self.getCentrality()[idx])
-
-    #    for runid in range(len(self.centList)):
-    #        info("\nStarting run " + str(runid) + "\n")
-    #        self.runId = str(runid)
-    #        if self.stop_all_nodes:
-    #            self.nodeCrashed = self.centList.pop(0)
-
-    #        if not self.startRun():
-    #            # some times process are not killed in time, UDP
-    #            # ports are still occupied and the next run can not
-    #            # start correctly. I kill everything, wait some time, try 
-    #            # to restart. If something still goes wrong i stop the emulation
-    #            self.killAll()
-    #            time.sleep(10)
-    #            info("\nWARNING: run_id " + str(runid) + " could not start, retrying...\n")
-    #            if not self.startRun():
-    #                error("\nERROR: run_id " + str(runid) + " could not start!" + \
-    #                        "please check the logs\n")
-    #                sys.exit(1)
+            if not self.startRun():
+                # some times process are not killed in time, UDP
+                # ports are still occupied and the next run can not
+                # start correctly. I kill everything, wait some time, try 
+                # to restart. If something still goes wrong i stop the emulation
+                self.killAll()
+                time.sleep(10)
+                info("\nWARNING: run_id " + str(run_id) + " could not start, retrying...\n")
+                if not self.startRun():
+                    error("\nERROR: run_id " + str(run_id) + " could not start!" + \
+                            "please check the logs\n")
+                    sys.exit(1)
 
 
 
-    #        eventDict = {
-    #                self.startLog:["Start logging", self.sendSignal,
-    #                    {"sig":signal.SIGUSR1}],
-    #                self.stopNode:["Stopping node(s) " + str(self.nodeCrashed) + " ",
-    #                    self.sendSignal, {"sig":signal.SIGTSTP,
-    #                    "hostName":self.nodeCrashed}],
-    #                self.stopLog:["Stop logging", self.sendSignal,
-    #                    {"sig":signal.SIGUSR1}]}
+            eventDict = {
+                    self.startLog:["Start logging (pinging)\n", self.start_ping, 
+                        {"exclude":self.nodeCrashed, "run_id":run_id}],
+                    self.stopLog:["Stopping logging ",
+                        self.sendSignal, {"sig":signal.SIGINT}],
+                    self.stopNode:["Stopping node(s) " + str(self.nodeCrashed) + "\n",
+                        self.sendSignal, {"sig":signal.SIGTERM, "hostName":self.nodeCrashed}]
+                        }
 
-    #        eventList = []
-    #        relativeTime = 0
-    #        for e in sorted(eventDict.keys()):
-    #            if e > 0:
-    #                data = eventDict[e]
-    #                eventList.append([e - relativeTime] + data)
-    #                relativeTime += (e - relativeTime)
+            eventList = []
+            relativeTime = 0
+            for e in sorted(eventDict.keys()):
+                if e > 0:
+                    data = eventDict[e]
+                    eventList.append([e - relativeTime] + data)
+                    relativeTime += (e - relativeTime)
 
-    #        waitTime = self.duration - relativeTime
+            waitTime = self.duration - relativeTime
 
-    #        for event in eventList:
-    #            sleep(event[0])
-    #            print event
-    #            event[2](**event[3])
-    #            info(event[1] + str(time.time()) + "\n")
-    #        sleep(waitTime)
-    #        for pid in self.pendingProc.keys():
-    #            self.sendSig(pid, signal.SIGTERM)
-    #        time.sleep(2)
-    #        # in case some process got stuck:
-    #        self.killAll()
-    #        time.sleep(2)
-    #        #sendSignal(signal.SIGUSR2)
-    #        #if self.startLog > 0:
-    #        #    duration -= self.startLog
-    #        #    sleep(self.startLog)
-    #        #    print "XX", self.startLog, duration, time.time()
-    #        #    # this is interpreted by the daemons as 
-    #        #    # "start (or stop) logging"
-    #        #    self.sendSignal(signal.SIGUSR1)
-    #        #    info("\nStart logging now!\n") 
-    #        #if self.stopNode > 0:
-    #        #    crashTime = self.stopNode - self.startLog
-    #        #    duration -= crashTime
-    #        #    sleep(crashTime)
-    #        #    print "XX", self.stopNode, duration, time.time()
-    #        #    # this is interpreted as "crash"
-    #        #    self.sendSignal(signal.SIGTSTP, self.nodeCrashed)
-    #        #    info("\nSent crash signal to node " + str(self.nodeCrashed))
-    #        #if self.stopLog > 0:
-    #        #    stopTime = self.stopLog - (self.startLog + self.stopNode)
-    #        #    duration -= stopTime
-    #        #    print "XX", stopTime, duration, time.time()
-    #        #    sleep(stopTime)
-    #        #    self.sendSignal(signal.SIGUSR1)
-    #        #    info("\nStop logging now!\n") 
-    #        #print "XX", time.time(), duration
-    #        #sleep(duration)
-    #        ## this is interpreted by the daemons as 
-    #        ## "restart a new run"
-    #        #self.sendSignal(signal.SIGUSR2)
-
-
+            for event in eventList:
+                sleep(event[0])
+                event[2](**event[3])
+                info(event[1] + str(time.time()) + "\n")
+            sleep(waitTime)
+            for pid in self.pendingProc.keys():
+                self.sendSig(pid, signal.SIGTERM)
+            time.sleep(2)
+            self.killAll()
+            time.sleep(2)
 
 
     def get_centrality(self):
@@ -189,11 +142,8 @@ class OLSRTest(dummyRoutingRandomTest):
         if rNode:
             info("\nChosen node " + str(rNode) + " to fail\n")
 
-        
         for h in host_list:
             intf = h.intfList()
-            info(str(intf))
-            info(str([i.name for i in intf]))
             intf_list =  ' '.join(["\"" + i.name + "\""  for i in intf])
             olsr_conf_file = self.prefix + h.name + ".conf"
             olsr_lock_file =  "/var/run/" + h.name + ".log"
@@ -213,20 +163,41 @@ class OLSRTest(dummyRoutingRandomTest):
             if self.dump:
                 self.launch_sniffer(h)
 
-        sleep(10)
-        if len(host_list) > 1:
-            for h in host_list:
-                intf = h.intfList()
-                while True:
-                    d = self.get_random_destination() 
-                    if d != intf[0].ip:
-                        break
-                self.launch_ping(h, d)
-
-
         if not self.nodeCrashed and rNode:
             self.nodeCrashed = [rNode]
         return launch_pid
+
+    def start_ping(self, exclude=[], number=1, run_id=0):
+
+        print exclude
+        host_list = self.getAllHosts()
+        exclude_ips = []
+        destinations = {}
+
+        for h in host_list:
+            if h.name in exclude:
+                exclude_ips.append(h.intfList()[0].ip)
+            else:
+                destinations[h] = []
+
+        for h in destinations:
+            intf = h.intfList()
+            counter = 1000
+            while len(destinations[h]) < self.NumPing:
+               d = self.get_random_destination() 
+               if d != intf[0].ip and d not in exclude_ips:
+                   destinations[h].append(d)
+               counter -= 1
+               if counter < 0:
+                   error("Can not find ping destination for host " + \
+                           h.name + " and NumPing " + str(self.NumPing) + \
+                           " your configuration may be wicked." + \
+                           "Nodes that will fail are:" + str(exclude))
+                   exit(1)
+
+        for (h,ip_list) in destinations.items():
+                self.launch_ping(h, ip_list, run_id=run_id)
+
 
     def get_random_destination(self):
 
@@ -280,6 +251,12 @@ class OLSRTest(dummyRoutingRandomTest):
         {
         }
         """
+
+
+        if "NumPing" in args.keys():
+            self.NumPing = int(args["NumPing"])
+        else:
+            self.NumPing = 1
 
         if "HelloInterval" in args.keys():
             self.HelloInterval = float(args["HelloInterval"])
