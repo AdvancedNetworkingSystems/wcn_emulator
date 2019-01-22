f4a3bd43a04a1c8b97993f1739d23e925afffcd6
Gabriele Gemmi <gabriel@autistici.org>
2019-01-22 21:47:03 +0100

    Separated script for analysis

--- analysis.py
+++ analysis.py
@@ -2,13 +2,21 @@ import sys
 import os
 import numpy as np
 import csv
+def mean_exp(path):
+    result = []
+    for p in ["NOPOP", "POP", "POPPEN"]:
+        with open("%s/%s/result.dat" % (path, p)) as f:
+            data = []
+            r = csv.reader(f)
+            for l in r:
+                data.append(int(l[0]))
+            result.append(np.mean(data))
+    return result
 
 
 def mean_val(subpath):
     breaks = []
     dirs = os.listdir(subpath)
-    if "result.dat" in dirs:
-        dirs.remove("result.dat")
     for node in dirs:
         with open("%s/%s/breakage.dat" % (subpath, node)) as f:
             data = []
@@ -21,25 +29,16 @@ def mean_val(subpath):
             m_route = max(data, key=lambda x: x['correct'])['correct'] #Search for the max number of route (right one)
             filtered = data[5:-5] #Remove all the data before the wait time and the last 10 seconds
             stable = sorted([d['timestamp'] for d in filtered if d['correct'] != m_route])  # filter all about the fluctuations
-            longest_seq = max(np.split(stable, np.where(np.diff(stable) != 5)[0]+1), key=len).tolist()
-            breakage = float(len(longest_seq))*0.5
-            breaks.append(breakage)
-            #p2rint "%.2f,%s" %(float(len(longest_seq)) * 0.5, node) #ds
-    return breaks
+            longest_seq = max(np.split(stable, np.where(np.diff(stable) != 1)[0]+5), key=len).tolist()
+            print "%.2f,%s" %(float(len(longest_seq)) * 0.5, node) #ds
+            # diff = [x1 - x2 - 1 for (x1, x2) in zip(stable[1:], stable[:-1])] #make a vector of differences
+            # print >> fw, max(diff)
 
 
-def main(path, n):
-    data = np.empty([n,3,26])
-    dirs = os.listdir(path)
-    dirs.sort()
-    i=0
-    for d in dirs[:n]:
-        j=0
-        for p in ["NOPOP", "POP", "POPPEN"]: 
-            breaks =  mean_val("%s/%s/%s" % (path, d, p))
-            data[i,j]=breaks
-            j+=1
-        i+=1
-    print np.mean(data, (2,0))
+def main(path):
+    for p in ["NOPOP", "POP", "POPPEN"]:
+        print(p)
+        mean_val("%s/%s" % (path, p))
+
 if __name__ == '__main__':
-    main(sys.argv[1], int(sys.argv[2]))
+    main(sys.argv[1])
